63443fe079b7bb283a0263b16bc1bad1645e2e52
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-11 11:50:59 +0300

    Добавлены User, ApplicationDbContext, стили main-page

---
+++ AwesomeNetwork35/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AwesomeNetwork35.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeNetwork35.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+
+        //    builder.ApplyConfiguration(new FriendConfiguration());
+        //    builder.ApplyConfiguration(new MessageConfuiguration());
+        //}
+    }
+}
--- AwesomeNetwork35/Program.cs
+++ AwesomeNetwork35/Program.cs
@@ -1,7 +1,23 @@
+using AwesomeNetwork35.Data;
+using AwesomeNetwork35.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection))
+    .AddIdentity<User, IdentityRole>(opts => {
+        opts.Password.RequiredLength = 5;
+        opts.Password.RequireNonAlphanumeric = false;
+        opts.Password.RequireLowercase = false;
+        opts.Password.RequireUppercase = false;
+        opts.Password.RequireDigit = false;
+    }).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -13,12 +29,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
