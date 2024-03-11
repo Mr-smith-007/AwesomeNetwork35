b7ecc5f6797f23c59fe18e7d453acee44c7d9e08
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-11 14:10:09 +0300

    LoginViwModel, RegisterViewModel, AccountManagerController добавлены

---
+++ AwesomeNetwork35/Controllers/Account/AccountManagerController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using AwesomeNetwork35.Data.UoW;
+using AwesomeNetwork35.Models.Users;
+using AwesomeNetwork35.ViewModels.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeNetwork35.Controllers.Account
+{
+    public class AccountManagerController : Controller
+    {
+        private IMapper _mapper;
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountManagerController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View("Home/Login");
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var user = _mapper.Map<User>(model);
+
+                var result = await _signInManager.PasswordSignInAsync(user.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("MyPage", "AccountManager");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        [Route("MyPage")]
+        [HttpGet]
+        public async Task<IActionResult> MyPage()
+        {
+            var user = User;
+
+            var result = await _userManager.GetUserAsync(user);
+
+            var model = new UserViewModel(result);
+
+            //model.Friends = await GetAllFriend(model.User);
+
+            return View("User", model);
+        }
+
+        //private async Task<List<User>> GetAllFriend()
+        //{
+        //    var user = User;
+
+        //    var result = await _userManager.GetUserAsync(user);
+
+        //    var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+        //    return repository.GetFriendsByUser(result);
+        //}
+
+        [Route("Logout")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
