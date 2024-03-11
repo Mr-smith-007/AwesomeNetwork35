edc2fc4678fae339534757f269cb2c0e80a68204
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-11 12:56:01 +0300

    Добавлен метод регистрации, добавлено отчество во вью регистрагции

--- AwesomeNetwork35/Controllers/Account/RegisterController.cs
+++ AwesomeNetwork35/Controllers/Account/RegisterController.cs
@@ -35,6 +35,29 @@ namespace AwesomeNetwork35.Controllers.Account
             return View("RegisterPart2", model);
         }
 
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(model);
 
+                var result = await _userManager.CreateAsync(user, model.PasswordReg);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View("RegisterPart2", model);
+        }
     }
 }
