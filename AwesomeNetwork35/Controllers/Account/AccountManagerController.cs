3a9efd349406a343fccd7264cc90674e9fde0c94
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-12 12:36:46 +0300

    Добавлены методы AddFriend и DeleteFriend

--- AwesomeNetwork35/Controllers/Account/AccountManagerController.cs
+++ AwesomeNetwork35/Controllers/Account/AccountManagerController.cs
@@ -192,5 +192,41 @@ namespace AwesomeNetwork35.Controllers.Account
             return repository.GetFriendsByUser(result);
         }
 
+        [Route("AddFriend")]
+        [HttpPost]
+        public async Task<IActionResult> AddFriend(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            repository.AddFriend(result, friend);
+
+
+            return RedirectToAction("MyPage", "AccountManager");
+        }
+
+        [Route("DeleteFriend")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteFriend(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            repository.DeleteFriend(result, friend);
+
+            return RedirectToAction("MyPage", "AccountManager");
+
+        }
+
     }
 }
