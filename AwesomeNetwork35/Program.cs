125d236783f8804b707104d0d1eda4d62f23fdbb
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-13 09:46:08 +0300

    Program: app.UseAuthorization перемещен после app.UseRouting

--- AwesomeNetwork35/Program.cs
+++ AwesomeNetwork35/Program.cs
@@ -44,13 +44,14 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
